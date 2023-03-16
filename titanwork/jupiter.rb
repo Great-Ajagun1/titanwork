798341ccb4f2588f951fe47b06cafec9e0fe8f44
Great-Ajagun1 <greatajagun@gmail.com>
2023-03-16 10:34:44 +0000

    changes made to content

---
+++ titanwork/jupiter.rb
@@ -0,0 +1,11 @@
+{
+    "Schedule":{
+        "AdjustForDST": true
+    },
+    "ScheduleStatus": {
+        "Last":"2016-10-04T10:15:00+00:00",
+        "LastUpdated":"2016-10-04T10:16:00+00:00",
+        "Next":"2016-10-04T10:20:00+00:00"
+    },
+    "IsPastDue":false
+}1
